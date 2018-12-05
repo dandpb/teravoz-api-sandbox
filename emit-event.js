71c49b7514be88e5324500a2e11690946262435f
Daniel Barreto <dandpb@gmail.com>
2018-12-05 18:30:17 -0200

    adicionado mais eventos

--- emit-event.js
+++ emit-event.js
@@ -2,24 +2,24 @@ const request = require('request');
 const events = require('./events');
 
 /**
-  Envio de eventos a cada 5 segundos para a URL 'webhook'
-  , e a cada 5 segundos manda um evento da lista `events.js`
-*/
-module.exports = function emitEvent(webhook, i=0) {
+ Envio de eventos a cada 5 segundos para a URL 'webhook'
+ , e a cada 5 segundos manda um evento da lista `events.js`
+ */
+module.exports = function emitEvent(webhook, i=events.length) {
   console.info(`Enviando evento `, events[i]);
 
   // Realiza o POST
   request.post({
-    url: webhook,
-    json: events[i]
-  })
-  .on('response', (response) => {
-    console.log(`POST status code response is ${response.statusCode}`);
-  });
+      url: webhook,
+      json: events[i]
+    })
+    .on('response', (response) => {
+      console.log(`POST status code response is ${response.statusCode}`);
+    });
 
   // Envia outro evento em 5 segundos
   setTimeout(() => {
-    emitEvent(webhook, ++i);
+    emitEvent(webhook, --i);
   }, 5000);
 
 }
--- events.js
+++ events.js
@@ -1,38 +1,16 @@
 module.exports = [
-  {
-    "call_id": "1463669263.30033",
-    "code": "123456",
-    "type": "call.new",
-    "direction": "inbound",
-    "our_number": "0800000000",
-    "their_number": "11999990000",
-    "timestamp": "2017-01-01T00:00:00Z"
-  },
-  {
-    "call_id": "1463669263.30033",
-    "code": "123456",
-    "type": "call.standby",
-    "direction": "inbound",
-    "our_number": "0800000000",
-    "their_number": "11999990000",
-    "timestamp": "2017-01-01T00:00:10Z"
-  },
-  {
-    "call_id": "1463669263.30033",
-    "code": "123456",
-    "type": "call.ongoing",
-    "direction": "inbound",
-    "our_number": "0800000000",
-    "their_number": "11999990000",
-    "timestamp": "2017-01-01T00:00:20Z"
-  },
-  {
-    "call_id": "1463669263.30033",
-    "code": "123456",
-    "type": "call.finished",
-    "direction": "inbound",
-    "our_number": "0800000000",
-    "their_number": "11999990000",
-    "timestamp": "2017-01-01T00:00:30Z"
-  }
+  { type: 'call.recording-available', call_id: '0003-1544025428.4450507', url: 'https://api.teravoz.com.br/recording/0003-1544025428.4450507.wav', timestamp: '2018-12-05T16:00:14.771Z' },
+  { type: 'actor.left', actor: 'emerson.morimoto@gradus-softwares.com.br', number: '100', timestamp: '2018-12-05T15:58:06.212Z', call_id: '0003-1544025428.4450507' },
+  { type: 'call.finished', call_id: '0003-1544025428.4450507', direction: 'inbound', our_number: '551131672542', their_number: '11984363878', timestamp: '2018-12-05T15:58:06.213Z' },
+  { type: 'actor.entered', actor: 'emerson.morimoto@gradus-softwares.com.br', number: '100', timestamp: '2018-12-05T15:57:50.791Z', call_id: '0003-1544025428.4450507', our_number: '551131672542', their_number: '11984363878' },
+  { type: 'call.ongoing', call_id: '0003-1544025428.4450507', direction: 'inbound', our_number: '551131672542', their_number: '11984363878', timestamp: '2018-12-05T15:57:50.792Z' },
+  { type: 'peer.ringing', number: '101', call_id: '0003-1544025428.4450507', timestamp: '2018-12-05T15:57:46.438Z' },
+  { type: 'peer.ringing', number: '100', call_id: '0003-1544025428.4450507', timestamp: '2018-12-05T15:57:46.128Z' },
+  { type: 'actor.ringing', actor: 'emerson.morimoto@gradus-softwares.com.br', number: '101', timestamp: '2018-12-05T15:57:45.557Z', call_id: '0003-1544025428.4450507', queue: '901', our_number: '551131672542', their_number: '11984363878' },
+  { type: 'actor.ringing', actor: 'emerson.morimoto@gradus-softwares.com.br', number: '100', timestamp: '2018-12-05T15:57:45.568Z', call_id: '0003-1544025428.4450507', queue: '901', our_number: '551131672542', their_number: '11984363878' },
+  { type: 'peer.ringing', number: '100', call_id: '0003-1544025428.4450507', timestamp: '2018-12-05T15:57:45.581Z' },
+  { type: 'peer.ringing', number: '101', call_id: '0003-1544025428.4450507', timestamp: '2018-12-05T15:57:45.578Z' },
+  { type: 'actor.ringing', actor: 'emerson.morimoto@gradus-softwares.com.br', number: '101', timestamp: '2018-12-05T15:57:45.555Z', call_id: '0003-1544025428.4450507', queue: '901', our_number: '551131672542', their_number: '11984363878' },
+  { type: 'call.waiting', call_id: '0003-1544025428.4450507', direction: 'inbound', our_number: '551131672542', their_number: '11984363878', timestamp: '2018-12-05T15:57:45.495Z' },
+  { type: 'call.new', call_id: '0003-1544025428.4450507', direction: 'inbound', from: '011984363878', our_number: '551131672542', their_number: '11984363878', their_number_type: 'mobile', timestamp: '2018-12-05T15:57:08.697Z' }
 ]
